28cfc7f9b0422901c6865c774b0c7f0573e292f4
mioppong <michaeloppong123@gmail.com>
2019-10-15 21:06:10 -0400

    given all the players, playing x minutes and averaging n points, this new functions print players points per minutes played

--- nba-analytics.py
+++ nba-analytics.py
@@ -4,7 +4,8 @@ import pandas as pd
 import numpy as np
 from collections import defaultdict
 import argparse
-
+from pprint import pprint
+import operator
 
 
 
@@ -25,7 +26,8 @@ def main():
 
     #uses nbastats2018-2019.csv
     #avg_20_plus(filename)
-    avg_20_plus_mins(filename)
+    #avg_20_plus_mins(filename)
+    points_per_minutes(filename)
   
 def avg_20_plus(filename):
     #returns players who avged more than 20 points
@@ -51,6 +53,27 @@ def avg_20_plus_mins(filename):
             print(player.Name)
     print(str(len(list_30_plus_mins))," players play 30 plus mins")
 
+
+def points_per_minutes(filename):
+    #given x minutes, and averaging n points
+    #the result is player y scored n/x points per minute
+    df = pd.read_csv(filename)
+
+    df['MP'] = df['MP'].astype(int)
+    df['Points'] = df['Points'].astype(int)
+
+    dict_player_and_ppm = {}
+    sorted_answer = {}
+    curr_pts_div_mins = 0
+    for index, player in df.iterrows():
+        if(player['MP']!=0):
+            curr_pts_div_mins = round(player['Points'] / player['MP'],2)
+            dict_player_and_ppm[player['Name']] =  curr_pts_div_mins
+
+    sorted_answer = sorted(dict_player_and_ppm.items(),key=operator.itemgetter(1))
+
+
+    pprint(sorted_answer[::-1])
 
 if __name__=="__main__":
     main()
